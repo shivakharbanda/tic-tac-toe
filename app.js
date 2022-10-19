22dcf2df5e153c092f99b60c7ec9753520a05618
shivakharbanda <shivakharbanda11@gmail.com>
2022-10-19 22:54:41 +0530

    AI finally made

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ let gameBoard = (function(){
     var gameBoardDisplay = () => {
 
         let gameboxes = [...document.querySelectorAll(".td")];
-        //console.log(gameboxes)
+        ////////console.log(gameboxes)
         gameboxes.forEach(Element=> {
             let index = Element.id;
 
-            //console.log(gameBoard.gameBoardArr);
+            ////////console.log(gameBoard.gameBoardArr);
             if (gameBoard.gameBoardArr[index] == "X"){
                 Element.textContent = "⨉"
             } else if (gameBoard.gameBoardArr[index] == "O") {
@@ -32,7 +32,7 @@ let gamePlayer = (name, symbol) => {
     let draws = 0;
 
     function getPlayerName() {
-        console.log(this);
+        //////console.log(this);
     }
     return {name, symbol, wins, losses, draws, getPlayerName};
 }
@@ -52,6 +52,7 @@ let displayController = (function () {
     let _selectionBtn2 = document.querySelector(".btn-2");
 
     let _playerNameBox = document.querySelector(".name-form");
+    let _playerNameAI = document.querySelector(".name-form-ai");
 
     let _player1 = "";
     let _player2 = "";
@@ -67,13 +68,12 @@ let displayController = (function () {
 
     var _congratulateWinner = (winnerName) => {
 
-
         
         playNextRoundBtn = document.createElement("button");
         playNextRoundBtn.classList.add("play-next-round")
         playNextRoundBtn.textContent = "Play Next Round"
 
-        //console.log("winnerbox");
+        ////////console.log("winnerbox");
         winnerBox = document.querySelector(".congratulations");
         winnerBox.textContent = "";
 
@@ -86,15 +86,15 @@ let displayController = (function () {
         } else {
             congratulationMsg.textContent = `congratulations ${winnerName}!`
         };
-       
-    
-        //console.log(referenceNode);
+
         winnerBox.appendChild(congratulationMsg);
+
+       
         winnerBox.appendChild(playNextRoundBtn);
 
         setTimeout(function(){
             playNextRoundBtn.addEventListener("click", () => {
-                setTimeout(5000);
+                
                 fullscreenbox.classList.add("dont-show");
             });
         }, 50);
@@ -105,7 +105,7 @@ let displayController = (function () {
 
     var _checkWinner = (whatMark) => {
     
-        //console.log(arr);
+       
         let winningConfigs = [
             [2, 5, 8],
             [0, 4, 8],
@@ -117,12 +117,8 @@ let displayController = (function () {
             [6, 7, 8]
         ]
 
-        for (i = 0; i < winningConfigs.length; i ++) {
-            //console.log(winningConfigs[i][0])
-
-            //console.log(arr[winningConfigs[i][0]]);
-            //console.log(whatMark);
-            //console.log(arr[winningConfigs[i][0]] == whatMark);
+        for (let i = 0; i < winningConfigs.length; i ++) {
+           
             if (gameBoard.gameBoardArr[winningConfigs[i][0]] == whatMark && gameBoard.gameBoardArr[winningConfigs[i][1]] == whatMark && gameBoard.gameBoardArr[winningConfigs[i][2]] == whatMark) {
 
                 return "win"
@@ -131,7 +127,7 @@ let displayController = (function () {
             
         };
         
-
+        return false;
     };
     function _nextStep() {
         //_gameDiv.classList.remove("dont-show");
@@ -139,6 +135,15 @@ let displayController = (function () {
         _welcomePanel.classList.add("dont-show");
         _playerNameBox.classList.remove("dont-show");
         _getPlayerNames();
+       
+    };
+
+    function _nextStepAI() {
+        //_gameDiv.classList.remove("dont-show");
+        
+        _welcomePanel.classList.add("dont-show");
+        _playerNameAI.classList.remove("dont-show");
+        _getPlayerNameAI();
        
     }
 
@@ -235,7 +240,6 @@ let displayController = (function () {
         }
     }
 
-
     function _HumanVHuman() {
         _renderNameAndStats();
 
@@ -249,7 +253,7 @@ let displayController = (function () {
                 if (chanceFlag && gameBoard.gameBoardArr[cell.id] == "") {
                     gameBoard.gameBoardArr[cell.id] = "X";
                     _count = _count + 1
-                    //console.log(gameBoard.gameBoardArr);
+                    //////console.log(gameBoard.gameBoardArr);
                     chanceFlag = false;
                 } else if (!chanceFlag && gameBoard.gameBoardArr[cell.id] == ""){
                     gameBoard.gameBoardArr[cell.id] = "O";
@@ -258,8 +262,8 @@ let displayController = (function () {
                 }
                 
                 gameBoard.gameBoardDisplay();
-                //console.log(chanceFlag);
-                //console.log(gameBoard.gameBoardArr)
+                //////console.log(chanceFlag);
+                ////console.log(gameBoard.gameBoardArr)
                 setTimeout(function(){
                     let winStatus = _checkWinner(chanceFlag?"O":"X");
 
@@ -295,7 +299,7 @@ let displayController = (function () {
                 }, 50);
 
                 
-                //console.log(_count);
+                ////console.log(_count);
                 
                 
 
@@ -303,24 +307,264 @@ let displayController = (function () {
         });
     };
 
+    function _getPlayerNameAI() {
+        
+        let _playerAIInput = document.getElementById("player-ai");
+
+        _playerAIInput.value = ""
+        
+
+        let _playBtn = document.querySelector(".play-game-btn-ai");
+        let _skipBtn = document.querySelector(".skip-btn-ai");
+
+        _skipBtn.addEventListener("click", () => {
+            _player1 = gamePlayer("Player 1", "X");
+            _player2 = gamePlayer("AI", "O");
+
+            _gameDiv.classList.remove("dont-show");
+            _playerNameAI.classList.add("dont-show");
+            
+            _playerAIInput.value = "";
+
+            _renderNameAndStats();
+        })
+
+
+        _playBtn.addEventListener("click", ()=>{
+            let _player1Name = _playerAIInput.value;
+            let _player2Name = "AI";
+
+            
+            if (_player1Name == "" || _player2Name == "") {
+                alert("field Cant be empty");
+            } else {
+                
+                _player1 = gamePlayer(_player1Name, "X");
+                _player2 = gamePlayer(_player2Name, "O");
+    
+                _gameDiv.classList.remove("dont-show");
+                _playerNameAI.classList.add("dont-show");
+            }
+
+            _playerAIInput.value = ""
+            _renderNameAndStats();
+        })
+        
+
+    };
+
+    function _checkDraw(state) {
+        
+        for (let i = 0; i < state.length; i ++) {
+            if (state[i] == "") {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    function _checkWin(state) {
+        
+        let winningConfigs = [
+            [2, 5, 8],
+            [0, 4, 8],
+            [0, 1, 2],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 4, 6],
+            [3, 4, 5],
+            [6, 7, 8]
+        ];
+
+        for (let i = 0; i < winningConfigs.length; i ++) {
+
+            if (_checkDraw(state)) {
+                
+                return ["DRAW", "none"]
+            }
+           
+            if (gameBoard.gameBoardArr[winningConfigs[i][0]] == "X" && gameBoard.gameBoardArr[winningConfigs[i][1]] == "X" && gameBoard.gameBoardArr[winningConfigs[i][2]] == "X") {
+
+                return ["win", "X"];
+            } else if (gameBoard.gameBoardArr[winningConfigs[i][0]] == "O" && gameBoard.gameBoardArr[winningConfigs[i][1]] == "O" && gameBoard.gameBoardArr[winningConfigs[i][2]] == "O") {
+
+                return ["win", "O"];
+            }
+
+            
+        };
+
+        return ["none", "nome2"]
+
+
+    }
+    
+    function GetRandomMove(state) {
+        let _bestScore = Infinity;
+        let _bestPosition;
+        for (let i = 0; i < state.length; i ++) {
+            
+            let score;
+            if (state[i] == "") {
+                state[i] = "O";
+                score = _miniMax(state, true);
+
+                
+                if (score < _bestScore) {
+                    _bestScore = score;
+                    _bestPosition = i
+                    //console.log(i)
+                }
+                state[i] = ""
+            }
+        }
+        
+        return _bestPosition;
+
+    };
+    function getScore(endWinner) {
+        if (endWinner == "X") {
+            return 1
+        } else if (endWinner == "O") {
+            return -1
+        } else if (endWinner == "none") {
+            return 0;
+        }
+    }
+    let _miniMax = (state, isMaximizing) => {
+
+        let _terminalConditionCheck;
+
+
+        _terminalConditionCheck = _checkWin(state);
+        ////console.log(state)
+        let _terminalConditionCheckResult = _terminalConditionCheck[0];
+        let _terminalConditionCheckWinner = _terminalConditionCheck[1];
+
+        if (_terminalConditionCheckResult == "win" || _terminalConditionCheckResult == "DRAW") {
+            let score = getScore(_terminalConditionCheckWinner);
+            return score;
+
+        } else if (isMaximizing) {
+            let score = -Infinity;
+            for (let i = 0; i < state.length; i ++) {
+                if (state[i] == "") {
+                    state[i] = "X";
+                    score = Math.max(score, _miniMax(state, false));
+                    state[i] = ""
+                }
+            }
+
+            return score
+        } else {
+            let score = Infinity;
+            for (let i = 0; i < state.length; i ++) {
+                if (state[i] == "") {
+                    state[i] = "O";
+                    score = Math.min(score, _miniMax(state, true));
+                    state[i] = "";
+                }
+            }
+            return score;
+        }
+        
+    }
+
+
+    function HumanVAi() {
+        _renderNameAndStats();
+
+        chanceFlag = true;
+
+        cells = [...document.querySelectorAll(".td")];
+        
+        cells.forEach(cell =>{
+            cell.addEventListener("click", ()=>{
+                
+                if (chanceFlag && gameBoard.gameBoardArr[cell.id] == "") {
+                    gameBoard.gameBoardArr[cell.id] = "X";
+                    _count = _count + 1
+                    
+                    chanceFlag = false;
+                    setTimeout(function () {
+                        if (!chanceFlag){
+                 
+                            _cellNo = GetRandomMove(gameBoard.gameBoardArr);
+                        
+                           
+                            gameBoard.gameBoardArr[_cellNo] = "O";
+                            _count = _count + 1
+                            chanceFlag = true;
+                            gameBoard.gameBoardDisplay();
+                        }
+                    }, 50);
+                };
+               
+              
+                setTimeout(function(){
+                    let winStatus = _checkWinner(chanceFlag?"O":"X");
+
+                    if (winStatus == "win" && chanceFlag == false) {
+                        _congratulateWinner(_player1.name);
+
+                        _resetBoard();
+                        
+                        _updateGameStats(_player1, 1, 0, 0);
+                        _updateGameStats(_player2, 0, 1, 0);
+
+                        
+
+                    } else if (winStatus == "win" && chanceFlag == true) {
+                        _congratulateWinner(_player2.name);
+                        ////console.log("coming here");
+                        _resetBoard();
+
+                        _updateGameStats(_player2, 1, 0, 0);
+                        _updateGameStats(_player1, 0, 1, 0);
+                        
+                    } else if (_count == 9) {
+                        ////console.log(_count, "hereee")
+                        _congratulateWinner("DRAW");
+
+                        _resetBoard();
+
+                        _updateGameStats(_player1, 0, 0, 1);
+                        _updateGameStats(_player2, 0, 0, 1);
+
+                        
+                    };
+                    _renderNameAndStats();
+                }, 50);
+
+                
+                ////////console.log(_count);
+                
+                
+
+            });
+        });
+    }
+
     function startGame() {
         _selectionBtn1.addEventListener("click", ()=>{
             setTimeout(function(){
                 let gameMode = "HVH";
                 _nextStep();
                 _HumanVHuman();
-                //console.log(gameMode);
+                ////////console.log(gameMode);
                 }, 50);   
         });
-        
         _selectionBtn2.addEventListener("click", ()=>{
-            let gameMode = "HVA";
-
-            //console.log(gameMode);
-
+            setTimeout(function(){
+                let gameMode = "HVA";
+                _nextStepAI();
+                HumanVAi()
+                ////////console.log(gameMode);
+                }, 50);   
         });
     };
-    
+   
+
     return {startGame};
 
 })();
@@ -328,3 +572,4 @@ let displayController = (function () {
 
 
 displayController.startGame();
+
